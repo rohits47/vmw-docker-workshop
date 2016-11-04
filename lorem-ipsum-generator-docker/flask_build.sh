f5322dfa68ce818fb5b5f1a6cf2a96ddbe7bbc7d
Rohit Sanbhadti <sanbhadtirohit@vmware.com>
2016-11-04 15:34:06 -0700

    final

--- lorem-ipsum-generator-docker/flask_build.sh
+++
@@ -1,7 +0,0 @@
-docker stop flaskapp
-docker rm flaskapp
-docker exec flaskapp /bin/bash -c "pip install twitter"
-docker run --name flaskapp \
-    -p 80:80 \
-    -v `pwd`/backend/:/app \
-    -d jazzdd/alpine-flask
