214978d8a00f763a17db100f63544a0d94da2011
Haitao Huang <haitaoh@haitaoh-m02.vmware.com>
2016-11-04 11:47:11 -0700

    update the code and add more logs

--- constans.py
+++ constans.py
@@ -9,5 +9,17 @@ class TwitterOAuth:
     CONSUMER_KEY = "consumer_key"
     CONSUMER_SECRET = "consumer_secret"
 
-# The Yahoo! Where On Earth ID
-WODID = 23424975
+# The Yahoo! Where On Earth ID (SF)
+WODID = 2487956
+
+# twitter ID in html
+DATA_TWEET_ID = "data-tweet-id"
+TWEET_TEXT_CLASS = "TweetTextSize  js-tweet-text tweet-text"
+TWEET_NAME = "tweet_name"
+TWEET_CONTENT = "tweet_content"
+TWEET_ID_STR = "id_str"
+TWEET_TEXT = "text"
+NAME = "name"
+URL = "url"
+TRENDS = "trends"
+DATA_OUTPUT = "data.json"
